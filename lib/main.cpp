a6f82143a30fdbcf6d2789ebf7ab7ed601cf38db
Alvaro Arenas <alvaare@gmail.com>
2021-09-24 18:35:29 +0200

    random seed

--- lib/main.cpp
+++ lib/main.cpp
@@ -4,6 +4,7 @@
 #include "neural_network.hpp"
 
 int main(int argc, char* argv[]) {
+    srand((unsigned)time(NULL));
     Descriptor desc(argv[1]);
     Information info(desc);
     Neural_network nn(desc, info);
